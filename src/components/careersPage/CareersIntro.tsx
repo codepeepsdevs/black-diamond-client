79bc4196caea0b329966170df155ae6341173044
Emmanuel <139286913+imDmanuel@users.noreply.github.com>
2024-11-03 22:39:14 +0100

    changes

--- src/components/careersPage/CareersIntro.tsx
+++ src/components/careersPage/CareersIntro.tsx
@@ -15,51 +15,45 @@ export default function CareersIntro() {
   return (
     <Slider {...settings}>
       <Image
-        className="max-h-[calc(100vh_-_120px)]"
+        className="max-h-[calc(100vh_-_120px)] object-cover"
         src={CareersIntroSlide1}
         alt="Careers Intro Image"
         priority
-        objectFit="cover"
         quality={100}
       />
       <Image
-        className="max-h-[calc(100vh_-_120px)]"
+        className="max-h-[calc(100vh_-_120px)] object-cover"
         src={CareersIntroSlide2}
         alt="Careers Intro Image"
         priority
-        objectFit="cover"
         quality={100}
       />
       <Image
-        className="max-h-[calc(100vh_-_120px)]"
+        className="max-h-[calc(100vh_-_120px)] object-cover"
         src={CareersIntroSlide3}
         alt="Careers Intro Image"
         priority
-        objectFit="cover"
         quality={100}
       />
       <Image
-        className="max-h-[calc(100vh_-_120px)]"
+        className="max-h-[calc(100vh_-_120px)] object-cover"
         src={CareersIntroSlide4}
         alt="Careers Intro Image"
         priority
-        objectFit="cover"
         quality={100}
       />
       <Image
-        className="max-h-[calc(100vh_-_120px)]"
+        className="max-h-[calc(100vh_-_120px)] object-cover"
         src={CareersIntroSlide5}
         alt="Careers Intro Image"
         priority
-        objectFit="cover"
         quality={100}
       />
       <Image
-        className="max-h-[calc(100vh_-_120px)]"
+        className="max-h-[calc(100vh_-_120px)] object-cover"
         src={CareersIntroSlide6}
         alt="Careers Intro Image"
         priority
-        objectFit="cover"
         quality={100}
       />
     </Slider>
