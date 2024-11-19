587d94549775373476cdd586b793855e4410e905
Emmanuel <139286913+imDmanuel@users.noreply.github.com>
2024-11-19 01:19:00 +0100

    preloader colour changes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,7 @@ import { Preloader2 } from "../../public/gif";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex flex-col gap-[1.2rem] items-center justify-center bg-[#020202] z-50 min-h-screen">
+    <div className="fixed inset-0 flex flex-col gap-[1.2rem] items-center justify-center bg-black z-50 min-h-screen">
       {/* <video
         className="w-[70%] xs:w-[60%] sm:w-[50%] md:w-[40%] lg:w-[30%] object-cover"
         preload="auto"
